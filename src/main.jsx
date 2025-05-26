28e97d9fca711405b058e9a11452579f85780f94
Krishna191919 <krishnachaudhary191919@gmail.com>
2025-05-26 14:08:40 +0545

    improved

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import Hero from "./components/Hero.jsx";
-import About from "./components/about.jsx";
+import About from "./components/About.jsx";
 import Product from "./components/Product.jsx";
 import "./index.css";
 import Services from "./components/Services.jsx";
